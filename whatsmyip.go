a7661c1e8a203db2141787afc48be764d31509eb
Claes Persson <c@cp.se>
2018-03-31 22:56:24 +0000

    alternativ metod

--- whatsmyip.go
+++ whatsmyip.go
@@ -1,15 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"strings"
+	"net"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "%s\n", r.RemoteAddr[:strings.Index(r.RemoteAddr, ":")])
-	})
+	l, err := net.Listen("tcp4", "0.0.0.0:8081")
+	if err != nil {
+		return
+	}
+	defer l.Close()
 
-	http.ListenAndServe(":8081", nil)
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			return
+		}
+
+		go func(conn net.Conn) {
+			a := []byte("HTTP/1.0 200 OK\nConnection: close\nContent-Type: text\n\n")
+			t := conn.RemoteAddr().String()
+			i := 0
+			v6 := false
+			if t[0] == '[' {
+				v6 = true
+				i = 1
+			}
+
+			for j := i; j < len(t); j++ {
+				ch := t[j]
+				if !v6 {
+					if ch == ':' {
+						break
+					}
+				} else {
+					if ch == ']' {
+						break
+					}
+				}
+
+				a = append(a, byte(ch))
+			}
+			a = append(a, byte('\n'))
+
+			conn.Write(a)
+			conn.Close()
+		}(c)
+	}
 }
